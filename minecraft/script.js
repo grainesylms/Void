27d5b4e0490f2eaa91e59204898c3c76d36e009f
SpeedSlicer <143555953+SpeedSlicer@users.noreply.github.com>
2025-04-30 18:18:19 -0400

    upgrade

--- minecraft/script.js
+++ minecraft/script.js
@@ -1,39 +1,4 @@
-const themes = {
-  starfall: "resources/backgrounds/starfall.gif",
-  
-};
 
-document.addEventListener("DOMContentLoaded", () => {
-  const selectedTheme = getCookie("theme") || "starfall";
-  const baseColor = getCookie("base-color") || "#6e6e6e";
-  const darkerColor = getCookie("darker-color") || "#5c5c5c";
-  const lighterColor = getCookie("lighter-color") || "#7f7f7f";
-  const highlightColor = getCookie("highlight-color") || "#a3a3a3";
-  const shadowColor = getCookie("shadow-color") || "#4b4b4b";
-  const textColor = getCookie("text-color") || "#ffffff";
-  const backgroundColor = getCookie("background-color") || "#1e1e1e";
-
-  document.querySelector(
-    ".background"
-  ).style.backgroundImage = `url(${themes[selectedTheme]})`;
-  document.getElementById("theme-select").value = selectedTheme;
-
-  updateColor("--base-color", baseColor);
-  updateColor("--darker-color", darkerColor);
-  updateColor("--lighter-color", lighterColor);
-  updateColor("--highlight-color", highlightColor);
-  updateColor("--shadow-color", shadowColor);
-  updateColor("--text-color", textColor);
-  updateColor("--background-color", backgroundColor);
-
-  document.getElementById("base-color-picker").value = baseColor;
-  document.getElementById("darker-color-picker").value = darkerColor;
-  document.getElementById("lighter-color-picker").value = lighterColor;
-  document.getElementById("highlight-color-picker").value = highlightColor;
-  document.getElementById("shadow-color-picker").value = shadowColor;
-  document.getElementById("text-color-picker").value = textColor;
-  document.getElementById("background-color-picker").value = backgroundColor;
-});
 
 function changeTheme() {
   const selectedTheme = document.getElementById("theme-select").value;
